78e5063d95f25bb67dd47cd0859a2a8361ad294b
sam-glow <sam@glowmade.com>
2023-09-21 17:00:06 +0100

    I cast Contingency

--- battle_wizard/Assets/Scripts/wizzard_prefabs.cs
+++ battle_wizard/Assets/Scripts/wizzard_prefabs.cs
@@ -7,12 +7,24 @@ public class wizzard_prefabs : MonoBehaviour
     public GameObject prefab_wizzard_01_left;
     public GameObject prefab_wizzard_01_right;
 
+    public GameObject prefab_wizzard_02_left;
+    public GameObject prefab_wizzard_02_right;
+
+    public GameObject prefab_wizzard_03_left;
+    public GameObject prefab_wizzard_03_right;
+
     public GameObject GetPrefab(int char_idx, int player_idx)
     {
         if (char_idx == 0)
         {
             return player_idx == 0 ? prefab_wizzard_01_left : prefab_wizzard_01_right;
         }
+
+        if(char_idx == 1)
+            return player_idx == 0 ? prefab_wizzard_02_left : prefab_wizzard_02_right;
+
+        if (char_idx == 2)
+            return player_idx == 0 ? prefab_wizzard_03_left : prefab_wizzard_03_right;
 
         return null;
     }
